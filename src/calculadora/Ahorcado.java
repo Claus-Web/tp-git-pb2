1c8388dd9d9aa90a860df2c9a5d0d237728f40af
JoseUnlam <h_joseunlam@hotmail.com>
2020-04-17 18:24:13 -0300

    # WARNING: head commit changed in the meantime
    actualizacion

--- src/calculadora/Ahorcado.java
+++ src/calculadora/Ahorcado.java
@@ -1,36 +1,52 @@
 import java.util.Scanner; 
- 
-public class Ahorcado { 
- 
-    public static void main(String[] args) {             
-    	Integer a;             
-    	Integer b;            
-    	Integer opcion;             
-    	Integer resultado;
-    	
-    	
-    	do{            
-    		System.out.println("Ingrese su operación /n 1 para sumar "
-    				+ "/n 2 para restar /n 3 para multiplicar /n 4 para dividir");            
-    		opcion = teclado.next();             
-    		}while(opcion < 1 && opcion > 4) 
- 
-            System.out.println("Ingrese el primer numero");             
-    	    a = teclado.next();              
-    	    System.out.println("Ingrese el segundo numero");             
-    	    a = teclado.next(); 
- 
-            if(opcion.equals(1)){                 
-            	resultado = a + b;                            
-            	}else if(opcion.equals(2)){                
-            		resultado = a - b;            
-            		}else if(opcion.equals(3)){                
-            			resultado = a * b;             
-            			}else{                
-            				resultado = a / b;            
-            				} 
- 
-            System.out.println("El resultado es " + resultado.toString());     
-    
-          }
-    } 
+	 
+	public class Ahorcado { 
+	 
+	    public static void main(String[] args) {             
+	    	Integer a = 0;             
+	    	Integer b = 0;            
+	    	Integer opcion;             
+	    	Integer resultado;
+	    	
+	    	Scanner teclado = new Scanner(System.in);
+	    	do{            
+	    		System.out.println("Ingrese su operación /n 1 para sumar "
+	    				+ "/n 2 para restar /n 3 para multiplicar /n 4 para dividir");            
+	    		opcion = teclado.nextInt();             
+	    		 
+	 
+	            System.out.println("Ingrese el primer numero");             
+	    	    a = teclado.nextInt();              
+	    	    System.out.println("Ingrese el segundo numero");             
+	    	    b = teclado.nextInt(); 
+	    	    switch(opcion) {
+	    	    case 1:
+	    	    resultado = a + b;
+	    	    System.out.println("El resultado es " + resultado.toString());
+	    	    break;
+	    	    case 2:
+	        	    resultado = a - b;
+	        	    System.out.println("El resultado es " + resultado.toString());
+	        	    break;
+	        	   
+	    	    case 3:
+	        	    resultado = a / b;
+	        	    System.out.println("El resultado es " + resultado.toString());
+	        	    break;
+	        	    
+	    	   case 4:
+	            resultado = a * b;
+	            System.out.println("El resultado es " + resultado.toString());
+	        	   break;
+	           
+	    	  default: 
+	    	   System.out.println("Nose puede realizar la operacion");
+	    	 }
+	            
+	    	}while(opcion < 1 && opcion > 4);
+	            //System.out.println("El resultado es " + resultado.toString());     
+	            
+	            teclado.close();
+	          }
+	    } 
+	
